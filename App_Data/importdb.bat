50af2e62877919490ed666d72ed36a95e191c6a2
bluegrizzly <alain.dessureaux@gmail.com>
2015-01-18 20:02:29 -0500

    db export script

--- App_Data/importdb.bat
+++ App_Data/importdb.bat
@@ -1,5 +1,4 @@
 @echo off 
-set root=C:\WineMapPub\app_data
-cd %root%
-mysql -u root  -e "CREATE DATABASE IF NOT EXISTS wineman";
-mysql -u root  wineman < *.sql
+rem "C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql" -ppiepasri -u root  -e "CREATE DATABASE IF NOT EXISTS wineman";
+
+"C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql" -ppiepasri -u root wineman < %~dp0\dumpdb.sql
